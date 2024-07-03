3988f09789c93b7409f4c93a4e294797e13c03fa
Leila Troxell <fxm6jn@virginia.edu>
2024-07-02 21:23:59 -0400

    does not work on playing page though

--- math-game/src/Game.js
+++ math-game/src/Game.js
@@ -2,9 +2,10 @@ import React from "react";
 import { useState, useEffect, useRef } from "react";
 import { GameLogic } from "./GameLogic";
 import { Arithmetic } from "./Arithmetic";
-import Modal from './Modal';
+import Modal from "./Modal";
 import HomeButton from "./HomeButton";
 import CountdownTimer from "./CountdownTimer";
+import Header from "./Header";
 
 function Game(operator) {
   const [lives, setLives] = useState(3);
@@ -18,8 +19,8 @@ function Game(operator) {
   const [restart, setRestart] = useState(false);
   const [startTimer, setStartTimer] = useState(false);
   const MULTIPLIER = 10;
-  const UNSELECTED_COLOR = '#D3D3D3';
-  const SELECTED_COLOR = '#85A1EF';
+  const UNSELECTED_COLOR = "#D3D3D3";
+  const SELECTED_COLOR = "#85A1EF";
 
   useEffect(() => {
     resetBoard();
@@ -29,48 +30,47 @@ function Game(operator) {
 
   useEffect(() => {
     if (selected === 3) {
-        checkAnswer();
-        // UNSELECT ANSWERS
+      checkAnswer();
+      // UNSELECT ANSWERS
     }
   }, [selected]);
 
   useEffect(() => {
     if (lives === 0) {
-        setGameOver(true);
+      setGameOver(true);
     }
   }, [lives]);
 
   useEffect(() => {
-    console.log("Time out!")
+    console.log("Time out!");
     if (timeOut === true) {
-        setGameOver(true);
+      setGameOver(true);
     }
   }, [timeOut]);
 
   const hideModal = () => {
     if (showInstructions === true) {
-        setShowInstructions(false);
-        setStartTimer(true);
+      setShowInstructions(false);
+      setStartTimer(true);
     } else if (gameOver === true) {
-        setGameOver(false);
-        resetBoard();
-        setLives(3);
-        setPoints(0);
-        setRestart(!restart);
-        setStartTimer(false);
+      setGameOver(false);
+      resetBoard();
+      setLives(3);
+      setPoints(0);
+      setRestart(!restart);
+      setStartTimer(false);
     }
-    
   };
 
   function resetBoard() {
     generateNumbers();
     setSelected(0);
     for (let i = 0; i < 16; i++) {
-        let updatedValue = {[i]: UNSELECTED_COLOR};
-        setItemColor(items => ({
-            ...items,
-            ...updatedValue
-        }));
+      let updatedValue = { [i]: UNSELECTED_COLOR };
+      setItemColor((items) => ({
+        ...items,
+        ...updatedValue,
+      }));
     }
   }
 
@@ -79,25 +79,24 @@ function Game(operator) {
     let selectedAnswerIndex = [];
     let selectedAnswer = [];
     for (const key in itemColor) {
-        if (itemColor[key] === SELECTED_COLOR) {
-            selectedAnswerIndex = [...selectedAnswerIndex, Number(key)];
-            selectedAnswer = [...selectedAnswer, numbers[Number(key)]["newNumber"]];
-        }
+      if (itemColor[key] === SELECTED_COLOR) {
+        selectedAnswerIndex = [...selectedAnswerIndex, Number(key)];
+        selectedAnswer = [...selectedAnswer, numbers[Number(key)]["newNumber"]];
+      }
     }
     console.log(selectedAnswerIndex);
     console.log(selectedAnswer);
     if (gameLogic.correctAnswer(selectedAnswerIndex)) {
-        setPoints(points + 100);
-        await delay(400);
-        resetBoard();
+      setPoints(points + 100);
+      await delay(400);
+      resetBoard();
     } else {
-        
-        setLives(lives - 1);
-    };
+      setLives(lives - 1);
+    }
   }
 
   const delay = (delayInms) => {
-    return new Promise(resolve => setTimeout(resolve, delayInms));
+    return new Promise((resolve) => setTimeout(resolve, delayInms));
   };
 
   function generateNumbers() {
@@ -105,14 +104,16 @@ function Game(operator) {
     let startingNumbers = [];
     const combiner = Arithmetic(operator);
     for (let i = 0; i < 8; i++) {
-        let newNumber = Math.round(Math.random() * MULTIPLIER);
-        startingNumbers.push(newNumber)
-        for (let j = i-1; j >= 0; j--) {
-            startingNumbers.push(combiner.operate(newNumber, startingNumbers[j]));
-        }
+      let newNumber = Math.round(Math.random() * MULTIPLIER);
+      startingNumbers.push(newNumber);
+      for (let j = i - 1; j >= 0; j--) {
+        startingNumbers.push(combiner.operate(newNumber, startingNumbers[j]));
+      }
     }
     for (let i = 0; i < 16; i++) {
-      let whichNumberIndex = Math.round(Math.random() * (startingNumbers.length-1));
+      let whichNumberIndex = Math.round(
+        Math.random() * (startingNumbers.length - 1)
+      );
       let newNumber = startingNumbers[whichNumberIndex];
       numbersWorking.push({ i, newNumber });
     }
@@ -131,54 +132,78 @@ function Game(operator) {
   }
 
   function answerSelected(event) {
-    const index = Number(event.currentTarget.getAttribute('index'));
+    const index = Number(event.currentTarget.getAttribute("index"));
     if (itemColor[index] === SELECTED_COLOR) {
-        setItemColor({...itemColor, [index]: UNSELECTED_COLOR});
-        setSelected(selected - 1);
+      setItemColor({ ...itemColor, [index]: UNSELECTED_COLOR });
+      setSelected(selected - 1);
     } else if (selected === 3) {
-        alert("You can only pick 3 numbers at a time!");
+      alert("You can only pick 3 numbers at a time!");
     } else {
-        setItemColor({...itemColor, [index]: SELECTED_COLOR});
-        setSelected(selected + 1);
+      setItemColor({ ...itemColor, [index]: SELECTED_COLOR });
+      setSelected(selected + 1);
     }
   }
 
   return (
     <div>
-        <h1>Welcome to {operator["operator"].charAt(0).toUpperCase() + operator["operator"].slice(1)}</h1>
-        <Modal show={showInstructions} handleClose={hideModal} homeOption={false}>
-            <p>
-                This game is played by selecting three numbers that are all touching each other.
-                The first two numbers selected must together equal the 3rd number when your selected
-                operator is used <br/><br/>
-                Collect as many points as you can within the 60 second timer <br/><br/>
-                3 mistakes are allowed before your attempt is ended for you
-            </p>
-        </Modal>
-        <Modal show={gameOver} handleClose={hideModal} homeOption={true}>
-            <h2>
-                Game Over! <br/><br/> You scored {points} points!
-            </h2>
-        </Modal>
-        {console.log("Game over status:")}
-        {console.log(startTimer)}
-        <HomeButton hidden={startTimer}/>
-        <div className="displayBoard">
-            <div className="pointsBlock">
-                <CountdownTimer initialSeconds={10} setTimeOut={setTimeOut} restart={restart} start={startTimer}/>
-                <p>Points: {points}</p>
-            </div>
-            <p className="livesBlock"> <br/> Lives: {lives}</p>
+      <Header />
+      <h1>
+        Welcome to{" "}
+        {operator["operator"].charAt(0).toUpperCase() +
+          operator["operator"].slice(1)}
+      </h1>
+      <Modal show={showInstructions} handleClose={hideModal} homeOption={false}>
+        <p>
+          This game is played by selecting three numbers that are all touching
+          each other. The first two numbers selected must together equal the 3rd
+          number when your selected operator is used <br />
+          <br />
+          Collect as many points as you can within the 60 second timer <br />
+          <br />3 mistakes are allowed before your attempt is ended for you
+        </p>
+      </Modal>
+      <Modal show={gameOver} handleClose={hideModal} homeOption={true}>
+        <h2>
+          Game Over! <br />
+          <br /> You scored {points} points!
+        </h2>
+      </Modal>
+      {console.log("Game over status:")}
+      {console.log(startTimer)}
+      <HomeButton hidden={startTimer} />
+      <div className="displayBoard">
+        <div className="pointsBlock">
+          <CountdownTimer
+            initialSeconds={10}
+            setTimeOut={setTimeOut}
+            restart={restart}
+            start={startTimer}
+          />
+          <p>Points: {points}</p>
         </div>
-        <div className="game-board" id="answerChoices">
+        <p className="livesBlock">
+          {" "}
+          <br /> Lives: {lives}
+        </p>
+      </div>
+      <div className="game-board" id="answerChoices">
         {numbers.map((number) => (
-            <div className="cell" key={number["i"]} style={{backgroundColor: itemColor[number["i"]]}}>
-                <button key={number["i"]} index={number["i"]} onClick={answerSelected} value={number["newNumber"]}>
-                    {number["newNumber"]}
-                </button>
-            </div>
+          <div
+            className="cell"
+            key={number["i"]}
+            style={{ backgroundColor: itemColor[number["i"]] }}
+          >
+            <button
+              key={number["i"]}
+              index={number["i"]}
+              onClick={answerSelected}
+              value={number["newNumber"]}
+            >
+              {number["newNumber"]}
+            </button>
+          </div>
         ))}
-        </div>
+      </div>
     </div>
   );
 }
